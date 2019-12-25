52c6cf18ef12f15fef3f5d5bffc25f336a410daf
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2019-12-25 22:23:28 +0100

    SOLID Design Principles - Single Responsibility Principle

---
+++ script.js
@@ -0,0 +1,19 @@
+import logMessage from './logger.js';
+
+class CaloriesTracker {
+    constructor(maxCalories) {
+        this.maxCalories = maxCalories;
+        this.currentCalories = 0;
+    }
+
+    trackCalories(caloriesCount) {
+        this.currentCalories += caloriesCount;
+
+        if (this.currentCalories > this.maxCalories) {
+            logMessage('Calories count exceeded!!!');
+        }
+    }
+}
+
+const caloriesTracker = new CaloriesTracker(2000);
+
